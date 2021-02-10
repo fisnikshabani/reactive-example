e1d426772ef85c347b30a40e66739b9a09449559
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-02-11 00:40:58 +0100

    Initial commit

---
+++ src/main/java/com/fisnik/reactive/Person.java
@@ -0,0 +1,19 @@
+package com.fisnik.reactive;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public String sayMyName(){
+        return "My name is: " + firstName + " " + lastName + "";
+    }
+
+}
